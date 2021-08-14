66d24e4565aa7d635717ab4a1625714a4e195c34
parksunwoo <psw261@gmail.com>
2021-08-15 00:00:30 +0900

    fix : add requirements.txt
    - comment pytorch_transformers
    - use cuda in transformation build_P_prime

--- deep-text-recognition-benchmark/modules/transformation.py
+++ deep-text-recognition-benchmark/modules/transformation.py
@@ -149,8 +149,8 @@ class GridGenerator(nn.Module):
         batch_inv_delta_C = self.inv_delta_C.repeat(batch_size, 1, 1)
         batch_P_hat = self.P_hat.repeat(batch_size, 1, 1)
         batch_C_prime_with_zeros = torch.cat((batch_C_prime, torch.zeros(
-            # batch_size, 3, 2).float().cuda()), dim=1)  # batch_size x F+3 x 2
-            batch_size, 3, 2).float()), dim=1)  # batch_size x F+3 x 2
+            batch_size, 3, 2).float().cuda()), dim=1)  # batch_size x F+3 x 2
+            # batch_size, 3, 2).float()), dim=1)  # batch_size x F+3 x 2
         batch_T = torch.bmm(batch_inv_delta_C, batch_C_prime_with_zeros)  # batch_size x F+3 x 2
         batch_P_prime = torch.bmm(batch_P_hat, batch_T)  # batch_size x n x 2
         return batch_P_prime  # batch_size x n x 2
